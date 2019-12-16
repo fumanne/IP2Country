227c20fe69af274bf283680481619e93096dd6d3
fu-l <fu-l@klab.com>
2019-12-16 15:58:10 +0800

    add test function

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"net"
 	"reflect"
 	"testing"
 )
@@ -14,7 +15,6 @@ func TestIP2long(t *testing.T) {
 
 }
 
-
 func TestLong2ip(t *testing.T) {
 	longip := int64(15191967)
 	v := reflect.TypeOf(Long2ip(longip))
@@ -22,4 +22,28 @@ func TestLong2ip(t *testing.T) {
 		t.Logf("%d convert ip type is OK", longip)
 	}
 
+}
+
+func TestIsIPv4(t *testing.T) {
+	right := "255.255.252.252"
+	wrong := "fe80::21b:77ff:fbd6:7860"
+	if IsIPv4(net.ParseIP(right)) {
+		t.Logf("%s is ipv4", right)
+	}
+
+	if IsIPv4(net.ParseIP(wrong)) {
+		t.Errorf("%s should not be ipv4", wrong)
+	}
+}
+
+func TestIsIPv6(t *testing.T) {
+	wrong := "255.255.252.252"
+	right := "fe80::21b:77ff:fbd6:7860"
+	if IsIPv6(net.ParseIP(wrong)) {
+		t.Errorf("%s should not be ipv6", wrong)
+	}
+
+	if IsIPv6(net.ParseIP(right)) {
+		t.Logf("%s is ipv6", right)
+	}
 }
